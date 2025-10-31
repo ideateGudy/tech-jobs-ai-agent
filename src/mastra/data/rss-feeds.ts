b089b50f6a681154369a6d2b2bcb6ceba7d701c4
Goodnews Azonubi <azonubigoodnews@gmail.com>
2025-10-31 18:48:08 +0100

    rss feeds

---
+++ src/mastra/data/rss-feeds.ts
@@ -0,0 +1,45 @@
+// Better tech-focused job RSS feeds
+// Note: These feeds have active tech roles and are regularly updated
+export const rssFeeds = [
+
+  // Smart Remote Jobs feed
+  "https://www.smartremotejobs.com/feed/all.rss",
+  "https://www.smartremotejobs.com/feed/software-development-remote-jobs.rss",
+  "https://www.smartremotejobs.com/feed/design-remote-jobs.rss",
+  "https://www.smartremotejobs.com/feed/project-management-remote-jobs.rss",
+  "https://www.smartremotejobs.com/feed/mobile-apps-remote-jobs.rss",
+
+  //Remotive.io feeds
+  "https://remotive.com/remote-jobs/feed", 
+  "https://remotive.com/remote-jobs/feed/software-dev", 
+  "https://remotive.com/remote-jobs/feed/devops", 
+  "https://remotive.com/remote-jobs/feed/design", 
+  "https://remotive.com/remote-jobs/feed/project-management", 
+  "https://remotive.com/remote-jobs/feed/product", 
+  "https://remotive.com/remote-jobs/feed/data",
+
+  //Himalayas.app tech job feed
+  "https://himalayas.app/jobs/rss", 
+
+  //We Work Remotely tech job feeds
+  "https://weworkremotely.com/remote-jobs.rss",
+  "https://weworkremotely.com/categories/remote-customer-support-jobs.rss", 
+  "https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss", 
+  "https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss", 
+  "https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss",
+  "https://weworkremotely.com/categories/remote-programming-jobs.rss", 
+  "https://weworkremotely.com/categories/remote-design-jobs.rss",
+
+  // Real Work From Anywhere tech job feed
+  "https://www.realworkfromanywhere.com/rss.xml",
+
+  // Empllo remote jobs feed
+  "https://empllo.com/feeds/remote-jobs.rss",
+  "https://empllo.com/feeds/remote-devops-jobs.rss",
+  "https://empllo.com/feeds/remote-design-jobs.rss",
+  "https://empllo.com/feeds/remote-product-jobs.rss",
+  "https://empllo.com/feeds/remote-data-jobs.rss",
+
+  // Jobicy tech jobs feed
+  "https://jobicy.com/feed/job_feed"
+];
